c532d0c3716d45f0d2efd0aa3bce1518b794fb68
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-07-30 22:54:55 +0300

    Completed Bot App Code Challenge

--- bot-battlr-app/src/components/BotCard.js
+++ bot-battlr-app/src/components/BotCard.js
@@ -1,19 +1,34 @@
 import React from 'react';
 
+// BotCard component that displays information about a bot
+// The component receives `bot` object and `onClick` function as props
 const BotCard = ({ bot, onClick }) => {
+  // Destructure properties from the `bot` object
   const { name, health, damage, armor, avatar_url } = bot;
 
+  // Render the bot card with its information
   return (
     <div className="bot-card" onClick={onClick}>
+      {/* Display the bot's avatar image */}
       <img src={avatar_url} alt={name} />
+
+      {/* Display the bot's information in a bot-info container */}
       <div className="bot-info">
+        {/* Display the bot's name */}
         <h3>{name}</h3>
+
+        {/* Display the bot's health */}
         <p>Health: {health}</p>
+
+        {/* Display the bot's damage */}
         <p>Damage: {damage}</p>
+
+        {/* Display the bot's armor */}
         <p>Armor: {armor}</p>
       </div>
     </div>
   );
 };
 
+// Export the BotCard component for use in other files
 export default BotCard;
