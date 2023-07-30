7eee085d64735703d30665fbe1cead1f019951b3
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-07-30 23:45:18 +0300

    Completed Bot App Code Challenge

--- bot-battlr-app/src/components/YourBotArmy.js
+++ bot-battlr-app/src/components/YourBotArmy.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import BotCard from './BotCard';
 
+// YourBotArmy component that displays enlisted bots in the user's army
+// The component receives `enlistedBots` and `releaseBot` as props
 const YourBotArmy = ({ enlistedBots, releaseBot }) => {
   return (
+    // Render a container for the user's enlisted bots
     <div className="your-bot-army">
+      {/* Heading to indicate the user's bot army */}
       <h2>Your Bot Army</h2>
+
+      {/* Container for the enlisted bots */}
       <div className="bot-list">
+        {/* Render each enlisted bot using the BotCard component */}
         {enlistedBots.map(bot => (
           <BotCard key={bot.id} bot={bot} onClick={() => releaseBot(bot.id)} />
         ))}
@@ -14,4 +21,5 @@ const YourBotArmy = ({ enlistedBots, releaseBot }) => {
   );
 };
 
+// Export the YourBotArmy component for use in other files
 export default YourBotArmy;
