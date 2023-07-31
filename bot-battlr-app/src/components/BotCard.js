db97ca86f8ac70585e218a709267cb10959fa19d
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-07-31 12:58:40 +0300

    Completed Bot App Code Challenge

--- bot-battlr-app/src/components/BotCard.js
+++ bot-battlr-app/src/components/BotCard.js
@@ -8,6 +8,7 @@ const BotCard = ({ bot, onClick }) => {
 
   // Render the bot card with its information
   return (
+    <div className='card'>
     <div className="bot-card" onClick={onClick}>
       {/* Display the bot's avatar image */}
       <img src={avatar_url} alt={name} />
@@ -17,19 +18,22 @@ const BotCard = ({ bot, onClick }) => {
         {/* Display the bot's name */}
         <h3>{name}</h3>
 
-        {/* Display the bot's health */}
-        <p>Health: {health}</p>
-
-        {/* Display the bot's damage */}
-        <p>Damage: {damage}</p>
-
-        {/* Display the bot's armor */}
-        <p>Armor: {armor}</p>
-
         {/* Display the bot's catchphrase */}
         <p>Catchphrase: {catchphrase}</p>
+
+        <hr />
+        
+        {/* Display the bot's health */}
+        <span><i class="bi bi-heart-pulse-fill"></i> {health}</span>
+
+        {/* Display the bot's damage */}
+        <span><i class="bi bi-lightning-fill"></i> {damage}</span>
+
+        {/* Display the bot's armor */}
+        <span><i class="bi bi-shield-shaded"></i> {armor}</span>
         
       </div>
+    </div>
     </div>
   );
 };
